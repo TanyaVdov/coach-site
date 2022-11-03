c510d143b0e6e1800ae7a1235c39985244e02820
TanyaVdov <tanme.vdov@gmail.com>
2022-11-03 12:49:35 +0200

    Add and configure multi-language (i18next)

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import {useTranslation} from 'react-i18next';
 
 const Main = () => {
+
+    const {t} = useTranslation();
+
     return (
-        <div>
-            <p>Main</p>
+        <div className='main'>
+
+            <div className='main_photo'>
+
+            </div>
+
+            <div className='main_content'>
+                <h1>{t('main.title')}</h1>
+                <p>{t('main.description_1')}</p>
+
+                <div className='repeat_button'>
+                    <button>Free...</button>
+                </div>
+
+                <p>{t('main.description_2')}</p>
+            </div>
+
         </div>
     );
 };

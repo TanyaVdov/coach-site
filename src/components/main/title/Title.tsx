5680277e17bc4c5741b5d57a5eb51415630ee067
TanyaVdov <tanme.vdov@gmail.com>
2022-11-21 16:30:47 +0200

    Styles of Main/Title in general

--- src/components/main/title/Title.tsx
+++ src/components/main/title/Title.tsx
@@ -14,31 +14,34 @@ const Title = () => {
     return (
         <Row className={s.row}>
 
-            <Col lg={4} className={s.imgCol}>
+            <Col md lg={4} className={s.imgCol}>
                 <img className={s.img}
                      src={img}
                      alt='img-title'
                 />
             </Col>
 
-            <Col lg={8} className={s.contentCol}>
+            <Col md lg={6} className={s.contentCol}>
+
                 <div className={s.content}>
 
                     <div className={s.title}>
                         <h1>{t('main.title')}</h1>
-                        <p>{t('main.desc1')}</p>
+                        <p>{t('main.subtitle')}</p>
                     </div>
 
-                    <div>
+                    <div className={s.button}>
                         <button>Free...</button>
                     </div>
 
-                    <div className={s.desc}>
-                        <p>{t('main.desc2')}</p>
+                    <div className={s.description}>
+                        <p>{t('main.description')}</p>
                     </div>
 
                 </div>
             </Col>
+
+            <Col md lg={2} />
 
         </Row>
     );

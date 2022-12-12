2be05e880c48c84445c753d45d53677221da2b78
TanyaVdov <tanme.vdov@gmail.com>
2022-12-12 17:37:44 +0200

    Added Goal modes with local state and hook

--- src/components/main/goal/GoalItem.tsx
+++ src/components/main/goal/GoalItem.tsx
@@ -2,7 +2,7 @@ import '../../../utils/i18next';
 import s from './goal.module.scss';
 
 import {useTranslation} from 'react-i18next';
-import React from "react";
+import React, {useState} from 'react';
 
 export type ItemPropsType = {
     img: string;
@@ -11,13 +11,33 @@ export type ItemPropsType = {
 
 const GoalItem = ({img, text}: ItemPropsType) => {
 
+    const [itemMode, setItemMode]= useState(false);
+
     const {t} = useTranslation();
 
+    const setTextMode = () => {
+        setItemMode(true);
+    }
+
+    const setImageMode = () => {
+        setItemMode(false);
+    }
+
     return (
-            <li className={s.goalItem} key={img}>
+        <li className={img}
+            key={img}
+            onMouseEnter={setTextMode}
+            onMouseLeave={setImageMode}>
+
+            {!itemMode &&
+                <img src={require(`../../../assets/icon/goal/${img}.png`)} alt={img}/>
+            }
+
+            {itemMode &&
                 <div className={s.text}>{t(text)}</div>
-                <img className={s.img} src={require(`../../../assets/icon/goal/${img}.png`)} alt={img}/>
-            </li>
+            }
+
+        </li>
     );
 };
 

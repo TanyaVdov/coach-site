ce4f948e011a771fd2ec5307332242b9d6c20222
TanyaVdov <tanme.vdov@gmail.com>
2022-12-19 21:10:34 +0200

    Added Resume modes with local state and hook

--- src/components/main/resume/Resume.tsx
+++ src/components/main/resume/Resume.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import '../../../utils/i18next';
 import s from "./resume.module.scss";
 
@@ -8,6 +8,16 @@ import {useTranslation} from "react-i18next";
 const Resume = () => {
 
     const {t} = useTranslation();
+
+    const [itemMode, setItemMode]= useState(false);
+
+    const setText3Mode = () => {
+        setItemMode(true);
+    }
+
+    const setText12Mode = () => {
+        setItemMode(false);
+    }
 
     return (
         <Row className={s.row}>
@@ -23,10 +33,22 @@ const Resume = () => {
             </Col>
 
             <Col lg={4} className={s.colContent}>
-                <div className={s.content}>
-                    <p className={s.text1}>{t('about.text1')}</p>
-                    <p className={s.text2}>{t('about.text2')}</p>
+                
+                <div 
+                    className={s.content}
+                    onMouseEnter={setText3Mode}
+                    onMouseLeave={setText12Mode}>
+
+                    {!itemMode && 
+                        <p className={s.text1}>{t('about.text1')}
+                            <p className={s.text2}>{t('about.text2')}</p>
+                        </p>
+                    }
+
+                    {itemMode &&
                     <p className={s.text3}>{t('about.text3')}</p>
+                    }
+
                 </div>
             </Col>
 

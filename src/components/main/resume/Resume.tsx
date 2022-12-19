c9ca5c396589d7dad2300a9f3a075fe0c323603b
TanyaVdov <tanme.vdov@gmail.com>
2022-12-19 20:54:26 +0200

    The component and the style of Resume-part

--- src/components/main/resume/Resume.tsx
+++ src/components/main/resume/Resume.tsx
@@ -1,10 +1,38 @@
 import React from "react";
+import '../../../utils/i18next';
+import s from "./resume.module.scss";
+
+import {Row, Col} from "react-bootstrap";
+import {useTranslation} from "react-i18next";
 
 const Resume = () => {
-    return (
-        <div>
 
-        </div>
+    const {t} = useTranslation();
+
+    return (
+        <Row className={s.row}>
+
+            <Col lg={2} />
+
+            <Col lg={4} className={s.colImg}>
+
+                <div className={s.name}>
+                    <h2>{t('about.name')}</h2>
+                    <p>{t('about.subname')}</p>
+                </div>
+            </Col>
+
+            <Col lg={4} className={s.colContent}>
+                <div className={s.content}>
+                    <p className={s.text1}>{t('about.text1')}</p>
+                    <p className={s.text2}>{t('about.text2')}</p>
+                    <p className={s.text3}>{t('about.text3')}</p>
+                </div>
+            </Col>
+
+            <Col lg={2} />
+
+        </Row>
     );
 };
 
